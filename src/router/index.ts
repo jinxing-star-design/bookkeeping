d6da7b3daa8070d19dbfa45834a49968e4b925c1
jinxing-star <jinxing97@126.com>
2021-02-25 15:05:10 +0800

    创建三个页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,44 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '@/views/Home.vue'
+import Money from '@/views/Money.vue';
+import Labels from '../views/Labels.vue';
+import Statistics from '../views/Statistics.vue'
 
 Vue.use(VueRouter)
 
-const routes: Array<RouteConfig> = [
+const routes  = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    redirect: '/money'
+    //默认访问的是money路径
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/money',
+    component: Money  
+    // 当用户访问这个路径时，要将money组件显示在App.vue中的router-view中
+  },
+  {
+    path: '/labels',
+    component: Labels
+  },
+  {
+    path:'/statistics',
+    component: Statistics
   }
+  // {
+  //   path: '/',
+  //   name: 'Home',
+  //   component: Home
+  // },
+  // {
+  //   path: '/about',
+  //   name: 'About',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  // }
 ]
 
 const router = new VueRouter({
