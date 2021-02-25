a96da690394a15a1326b21947daa922a34f9cd3e
jinxing-star <jinxing97@126.com>
2021-02-25 16:08:15 +0800

    添加404

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
-import Home from '@/views/Home.vue'
 import Money from '@/views/Money.vue';
 import Labels from '../views/Labels.vue';
 import Statistics from '../views/Statistics.vue'
+import { Component } from 'vue-property-decorator';
+import Notfound from '../views/Notfound.vue';
 
 Vue.use(VueRouter)
 
@@ -25,6 +26,10 @@ const routes  = [
   {
     path:'/statistics',
     component: Statistics
+  },
+  {
+    path:'*',
+    component: Notfound
   }
   // {
   //   path: '/',
